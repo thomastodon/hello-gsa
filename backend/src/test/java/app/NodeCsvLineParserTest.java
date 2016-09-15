b1aa07dd0b2a6708e2d436d0ac384ec0a6b9fcc4
thomasshouler <thomas.shouler@gmail.com>
2016-09-14 19:13:48 -0500

    refactored node line parser test

--- backend/src/test/java/app/NodeCsvLineParserTest.java
+++ backend/src/test/java/app/NodeCsvLineParserTest.java
@@ -1,44 +1,33 @@
-//package hello;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.runners.MockitoJUnitRunner;
-//
-//import static org.hamcrest.Matchers.is;
-//import static org.hamcrest.core.IsEqual.equalTo;
-//import static org.junit.Assert.assertThat;
-//import static org.mockito.Matchers.anyInt;
-//import static org.mockito.Mockito.doReturn;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class NodeCsvLineParserTest {
-//
-//    private NodeCsvLineParser subject;
-//
-//    @Before
-//    public void setup() {
-//        subject = new NodeCsvLineParser();
-//    }
-//
-//    @Test
-//    public void inputToDomain_translatesNode() {
-//        String[] nodeFields = new String[] {
-//                "NODE",
-//                "3327",
-//                "",
-//                "NO_RGB",
-//                "16.6620",
-//                "1.50000",
-//                "22.3340"
-//        };
-//
-//        NodeEntity node = subject.inputToDomain(new Structure(), nodeFields);
-//
-//        assertThat(node.getId(), is(equalTo(3327)));
-//        assertThat(node.getX(), is(equalTo(16.6620)));
-//        assertThat(node.getY(), is(equalTo(1.50000)));
-//        assertThat(node.getZ(), is(equalTo(22.3340)));
-//    }
-//}
+package app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NodeCsvLineParserTest {
+
+    @Test
+    public void inputToDomain_translatesNode() {
+        String[] nodeFields = new String[] {
+                "NODE",
+                "3327",
+                "",
+                "NO_RGB",
+                "16.6620",
+                "1.50000",
+                "22.3340"
+        };
+
+        NodeEntity node = NodeCsvLineParser.inputToDomain(nodeFields);
+
+        assertThat(node.getId(), is(equalTo(3327)));
+        assertThat(node.getX(), is(equalTo(16.6620)));
+        assertThat(node.getY(), is(equalTo(1.50000)));
+        assertThat(node.getZ(), is(equalTo(22.3340)));
+    }
+}
