bc0f6a922102975a1c9c9932ca1db3bc45d1b9a3
thomasshouler <thomas.shouler@gmail.com>
2016-09-18 20:48:54 -0500

    remove superfluous config annotations

--- backend/src/main/java/app/WebConfig.java
+++ backend/src/main/java/app/WebConfig.java
@@ -3,11 +3,9 @@ package app;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Value("${endpoints.cors.allowed-origins}")
