b272c8503a970c42ef93e2080086dcf887ab0070
thomasshouler <thomas.shouler@gmail.com>
2016-09-18 20:46:10 -0500

    add config annotation on integration tests

--- backend/src/test/java/app/StructureDaoTest.java
+++ backend/src/test/java/app/StructureDaoTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
+@WebAppConfiguration
 @IntegrationTest
 @ActiveProfiles("test")
 public class StructureDaoTest {
